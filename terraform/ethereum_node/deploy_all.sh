6f57f45b21d89594bb03a711f60bf41f218a085c
Pablo Castellano <pablo@anche.no>
2024-03-03 16:15:26 +0100

    added deploy_all.sh script for ethereum nodes

---
+++ terraform/ethereum_node/deploy_all.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+network=holesky
+export TF_WORKSPACE=ethereum-$network
+TF_VAR_network=$network terraform apply
+
+network=sepolia
+export TF_WORKSPACE=ethereum-$network
+TF_VAR_network=$network terraform apply
